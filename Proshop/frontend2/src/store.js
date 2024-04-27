cdece5b744213559eee138ded0af23a4ca88e7dc
blueIrisDevelopers <blueirisgroup6@gmail.com>
2024-04-27 15:21:28 +0300

    update of order fuctionality

--- Proshop/frontend2/src/store.js
+++ Proshop/frontend2/src/store.js
@@ -4,16 +4,32 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { 
+    orderDetailsReducer,
+    orderCreateReducer,
+    orderPayReducer,
+    orderListMyReducer,
+    orderListReducer,
+    orderDeliverReducer,
+} from './reducers/orderReducers'
 
 //Combine reducers into one central reducer
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
+    orderList: orderListReducer,
+    orderDeliver: orderDeliverReducer,
 });
 
 //Create the initial state of the store
@@ -23,9 +39,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
         JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? 
+        JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
+    cart: { 
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage,
+     },
     userLogin: { userInfo: userInfoFromStorage }
 }
 
